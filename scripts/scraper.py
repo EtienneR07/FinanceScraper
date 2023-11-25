d627bb77ce54210e01620040fbd21c3c23514f75
Etienne <etienner07@gmail.com>
2023-11-24 22:34:39 -0500

    Clean code and get more data

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -8,24 +8,48 @@ headers = {
 }
 
 
-def extract(symbol):
-    url = f"https://www.marketwatch.com/investing/stock/{symbol}/company-profile?mod=mw_quote_tab"
+def try_get(list, name, index, index2):
+    if index >= len(list):
+        return ''
+    else:
+        spans = list[index].find_all(name)
+        if index2 >= len(spans):
+            return ''
+        else:
+            return spans[index2].text
+
+
+def extractOverview(symbol):
+    url = f"https://www.marketwatch.com/investing/stock/{symbol}"
+    page = requests.get(url, headers=headers)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    list = soup.find("ul", "list list--kv list--col50").find_all('li')
+    open = try_get(list, 'span', 0, 0)
+    shares_outstanding = try_get(list, 'span', 4, 0)
+    eps = try_get(list, 'span', 9, 0)
+    return {
+        "symbol": symbol,
+        "openingPrice": open,
+        "sharesOutstanding": shares_outstanding,
+        "eps": eps
+    }
+
+
+def extractProfile(symbol):
+    url = f"https://www.marketwatch.com/investing/stock/{
+        symbol}/company-profile?mod=mw_quote_tab"
     page = requests.get(url, headers=headers)
     soup = BeautifulSoup(page.text, 'html.parser')
     tables = soup.find_all('table', 'table value-pairs no-heading')
-    valuationTableData = tables[0].find_all('td')
-    liquidityTableData = tables[2].find_all('td')
-    capitalizationTableData = tables[4].find_all('td')
-    pe = valuationTableData[1].text
-    priceToBook = valuationTableData[9].text
-    debtToEquity = capitalizationTableData[1].text
-    currentRatio = liquidityTableData[1].text
+    pe = try_get(tables, 'td', 0, 1)
+    price_to_book = try_get(tables, 'td', 0, 9)
+    debt_to_equity = try_get(tables, 'td', 4, 1)
+    current_ratio = try_get(tables, 'td', 2, 1)
     return {
-        "symbol": symbol,
         "pe": pe,
-        "priceToBook": priceToBook,
-        "debtToEquity": debtToEquity,
-        "currentRatio": currentRatio
+        "priceToBook": price_to_book,
+        "debtToEquity": debt_to_equity,
+        "currentRatio": current_ratio
     }
 
 
@@ -44,11 +68,15 @@ with open(stock_symbols_path, "r") as stock_symbols:
 
 for i in range(last_processed_index, len(lines)):
     try:
-        sleepTime = randint(8, 32)
-        time.sleep(sleepTime)
-        result = extract(lines[i].strip())
+        sleepTime1 = randint(5, 10)
+        sleepTime2 = randint(2, 6)
+        time.sleep(sleepTime1)
+        result_from_overview = extractOverview(lines[i].strip())
+        time.sleep(sleepTime2)
+        result_from_profile = extractProfile(lines[i].strip())
+        combined_data = {**result_from_overview, **result_from_profile}
         with open(file_path, 'a') as file:
-            file.write(f"{result},\n")
+            file.write(f"{combined_data},\n")
         with open(index_file_path, 'w') as file:
             file.write(str(last_processed_index))
         last_processed_index = last_processed_index + 1
